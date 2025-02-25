3098f01e02999031a6dfc2cf1ac834245436d135
Attica <roberts.justin180499@gmail.com>
2025-02-25 03:08:05 +0100

    .

--- Blauer-Marlin/obj/Release/net6.0/Blauer-Marlin.AssemblyInfo.cs
+++ Blauer-Marlin/obj/Release/net6.0/Blauer-Marlin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blauer-Marlin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52579bf86c3c7f2f99572e4fb7c0b49569e28fd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f51f63f9419bd8ba3dcddd57605907165f32e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blauer-Marlin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blauer-Marlin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
